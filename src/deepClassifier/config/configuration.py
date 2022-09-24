f407a3dd39d4d248b31a1ffe3241e4de1ab8213c
Shubham Rawat <rawatshbham09@gmail.com>
2022-09-24 10:26:58 +0530

    base model preparation completed and committed

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils import read_yaml, create_directories
-from deepClassifier.entity import DataIngestionConfig
+from deepClassifier.entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
+
 
 
 class ConfigurationManager:
@@ -26,3 +28,34 @@ class ConfigurationManager:
 
         return data_ingestion_config
 
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        return prepare_base_model_config
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+        return prepare_callback_config
